e5e91ce5399685341c9494cdfa5b47cb9affb460
eandstr <jae2meter@gmail.com>
2014-09-16 09:32:55 +0200

    dependencies

--- api.go
+++ api.go
@@ -1,29 +1,42 @@
 package main
 
 import (
-//	"fmt"
+	"fmt"
 	"net/http"
-//	"strconv"
+	"strconv"
 
-//	"github.com/codegangsta/martini"
+	"github.com/codegangsta/martini"
 )
 
 var portno = "66666\n"
 var portnoi = 0
 
-
-func GetPort(r *http.Request, enc Encoder) string {
-	// Get the query string arguments, if any
-	qs := r.URL.Query()
-	wasport := qs.Get("port")
-//	portnoi, err := strconv.Atoi(portno)
-//	if err != nil {
-//		portnoi = 0
-//	}
-	if wasport == "" {
-		return portno
+func SetPort (enc Encoder, parms martini.Params) (int, string) {
+        port, err := strconv.Atoi(parms["id"])
+	if err != nil || port > 65535{
+		return http.StatusNotFound, Must(enc.Encode(
+                        NewError(ErrCodeNotExist, fmt.Sprintf("Illegal portnumber %s ", parms["id"]))))
 	}
-	return "deFel"
+	return http.StatusOK, "helt OK"
 }
+
+
+
+
+
+
+// func GetPort(r *http.Request, enc Encoder) string {
+// 	// Get the query string arguments, if any
+// 	qs := r.URL.Query()
+// 	wasport := qs.Get("port")
+// //	portnoi, err := strconv.Atoi(portno)
+// //	if err != nil {
+// //		portnoi = 0
+// //	}
+// 	if wasport == "" {
+// 		return portno
+// 	}
+// 	return wasport
+// }
 
 
